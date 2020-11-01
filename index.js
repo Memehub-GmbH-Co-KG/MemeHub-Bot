0f80302b58e27bce28b92114d9942a5d1272dbd8
leifb <leif.bernsdorf@gmail.com>
2020-11-01 14:54:22 +0100

    Added env variables

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { Defaults } = require('redis-request-broker');
 Defaults.setDefaults({
     redis: {
-        prefix: 'mh:',
-        host: 'mhredis'
+        prefix: process.env.REDIS_PREFIX || 'mh:',
+        host: process.env.REDIS_HOST || "mhredis",
+        port: process.env.REDIS_PORT || undefined,
+        db: process.env.REDIS_DB || undefined,
+        password: process.env.REDIS_PASSWORD || undefined
     }
 });
 
